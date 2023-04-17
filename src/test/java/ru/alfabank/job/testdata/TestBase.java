43e8cb464f1d1c1d58a6e111b76b1307b3e902dc
Vladislav <molkov.udmnn@gmail.com>
2023-04-17 23:11:24 +0300

    add attach

--- src/test/java/ru/alfabank/job/testdata/TestBase.java
+++ src/test/java/ru/alfabank/job/testdata/TestBase.java
@@ -2,10 +2,12 @@ package ru.alfabank.job.testdata;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import ru.alfabank.job.config.ConfigReader;
 import ru.alfabank.job.config.ProjectConfiguration;
 import ru.alfabank.job.config.web.WebConfig;
+import ru.alfabank.job.helpers.Attach;
 
 public class TestBase {
     private static final WebConfig config = ConfigReader.Instance.read();
@@ -15,5 +17,13 @@ public class TestBase {
         SelenideLogger.addListener("allure",new AllureSelenide());
         ProjectConfiguration projectConfiguration = new ProjectConfiguration(config);
         projectConfiguration.webConfig();
+    }
+
+    @AfterEach
+    public void getAttach(){
+        Attach.addVideo();
+        Attach.browserConsoleLogs();
+        Attach.pageSource();
+        Attach.screenshotAs("ScreenShot");
     }
 }
